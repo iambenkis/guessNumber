c4c1aaab317087bd08445a87483330f9ff27667b
benkis <benkisenge03@gmail.com>
2022-04-28 00:13:21 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let toDisplay =  document.querySelector('.mynumber')
+let isNumber = document.querySelector('.numberis')
+let myNumber = document.querySelector('.foundnumber')
+let highscore = document.querySelector('.highscore')
+let inNumber = document.querySelector('.innumber');
+let checkBtn  = document.querySelector('.check')
+let againBtn  = document.querySelector('.again')
+const max = 20,min = 1;
+let randNumber = Math.floor(Math.random() * (max - min) ) + min
+checkBtn.addEventListener ('click',()=>{
+    
+})
+console.log(randNumber)
+
